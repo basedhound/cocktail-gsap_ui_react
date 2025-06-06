6a862e018545fed3113e959a91fe2dfa18e1dded
fvukelic <franck.vukelic@gmail.com>
2025-06-06 23:43:28 +0700

    Main commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import About from "./components/About";
+import Art from "./components/Art";
+import Cocktails from "./components/Cocktails";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
